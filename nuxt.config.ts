981f8f92144a2da3501d2ae2a71fe60930363850
kenan dufrene <kenan.dufrene@live.fr>
2024-10-20 16:39:34 +0200

    dev_install : install postcss / sass / tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+
+  /*
+  *   Remove autoPrefix for components
+  */
+  components: {
+    dirs: [{ path: '~/components', pathPrefix: false }]
+  },
+
+  css: [
+    '@/assets/scss/main.scss'
+  ],
+
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
 })
